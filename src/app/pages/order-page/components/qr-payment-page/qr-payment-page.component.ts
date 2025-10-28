b56a817c52b1d541a66c856817b6c0a133837c41
Ralph Brabante <ralph.webdev@gmail.com>
2025-10-29 02:05:14 +0800

    Fixed

--- src/app/pages/order-page/components/qr-payment-page/qr-payment-page.component.ts
+++ src/app/pages/order-page/components/qr-payment-page/qr-payment-page.component.ts
@@ -202,6 +202,10 @@ export class QrPaymentPageComponent extends BaseComponent implements OnInit {
         .pipe(takeUntil(this.unsubscribe))
         .subscribe({
           next: (resp) => {
+            if (resp.data.attributes.last_payment_error !== null) {
+              window.location.href = `http://localhost:4200/order/payment-status?payment_intent_id=${resp.data.id}`;
+            }
+
             if (resp.data.attributes.next_action?.redirect) {
               window.location.href =
                 resp.data.attributes.next_action?.redirect.url;
